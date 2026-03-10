c0b01cdfe5f3bcef693fa474e3d0029911657a58
SaiTangirala <Tangiralatarun8@gmail.com>
2026-03-10 22:58:14 +0000

    Update Roboticexplorer.py
    Rover navigates waypoints, analyses rocks at sites B/C/D/E, transmits data. Plan cost: 13 steps (G→E→G→B→C→D).

--- Roboticexplorer.py
+++ Roboticexplorer.py
@@ -1,12 +1,9 @@
 """
-Domain PDDL
+Domain
 """
-domain =
-(define (domain explorer_domain)
+(define (domain robotic_explorer_domain)
   (:requirements :strips)
-
   (:types waypoint)
-
   (:predicates
     (at ?w - waypoint)
     (connected ?w1 ?w2 - waypoint)
@@ -14,19 +11,16 @@ domain =
     (analysed ?w - waypoint)
     (transmitted ?w - waypoint)
   )
-
   (:action move
     :parameters (?from ?to - waypoint)
     :precondition (and (at ?from) (connected ?from ?to))
     :effect (and (not (at ?from)) (at ?to))
   )
-
   (:action analyse_rocks
     :parameters (?w - waypoint)
     :precondition (and (at ?w) (rocks_at ?w))
     :effect (analysed ?w)
   )
-
   (:action transmission
     :parameters (?w - waypoint)
     :precondition (and (at ?w) (analysed ?w))
@@ -35,38 +29,28 @@ domain =
 )
 
 """
-Problem PDDL
+Problem
 """
-(define (problem explorer_problem)
-  (:domain explorer_domain)
+(define (problem robotic_explorer_problem)
+  (:domain robotic_explorer_domain)
   (:requirements :strips)
-
   (:objects A B C D E F G - waypoint)
-
   (:init
     (at G)
-    (connected A B)
-    (connected B C)
-    (connected C B)
-    (connected C D)
-    (connected D C)
-    (connected D E)
-    (connected E G)
-    (connected G E)
-    (connected B G)
-    (connected G B)
-    (connected G F)
-    (connected F A)
-    (rocks_at B)
-    (rocks_at C)
-    (rocks_at D)
-    (rocks_at E)
+    (connected A B) (connected B A)
+    (connected B C) (connected C B)
+    (connected C D) (connected D C)
+    (connected D E) (connected E D)
+    (connected E G) (connected G E)
+    (connected B G) (connected G B)
+    (connected G F) (connected F G)
+    (connected F A) (connected A F)
+    (rocks_at B) (rocks_at C) (rocks_at D) (rocks_at E)
   )
-
   (:goal
     (forall (?w - waypoint)
-      (or (not (rocks_at ?w))
-          (transmitted ?w))
+      (or (not (rocks_at ?w)) (transmitted ?w))
     )
   )
 )
+
